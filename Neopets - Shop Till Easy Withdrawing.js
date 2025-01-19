24cc3377960b432852412868b1e72ec14296d32d
0o0slytherinpride0o0 <hillary.animenerd@gmail.com>
2025-01-18 22:50:31 -0500

    Add files via upload

--- Neopets - Shop Till Easy Withdrawing.js
+++ Neopets - Shop Till Easy Withdrawing.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @author         0o0slytherinpride0o0
+//                 With one line taken from diceroll123's script
 // @name           Neopets - Shop Till Easy Withdrawing
 // @description    Fills the textbox with the amount in your shop till when you click on the cell that says "Amount in NP"
 //                 Click again to clear
@@ -17,6 +18,8 @@ function fillorclear(object, n) {
 
 var till_table = document.querySelector('[align="center"][cellpadding="3"][cellspacing="0"][border="0"]');
 var till_textbox = document.querySelector('[type="text"][name="amount"][size="10"][maxlength="10"]');
+
+// took this from diceroll123's script
 var till_amount = document.body.innerHTML.match(/You currently have <b>([0-9,\,]*) NP<\/b> in your till./)[1].replace(/,/g, '');
 
 till_table.children[1].children[0].children[0].addEventListener("click", function () { fillorclear(till_textbox, till_amount) });
